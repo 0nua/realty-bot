ae41f91f8434bd071794edcca05f0e81df2aff38
Vladimir Prisada <vladimir-prisada@yandex.ru>
2023-03-18 13:24:31 +0100

    Improve error handler

--- src/service/errorHandler.ts
+++ src/service/errorHandler.ts
@@ -1,11 +1,14 @@
-import tgBot from "./tgBot";
+import {Telegraf} from "telegraf";
 
 export default class ErrorHandler {
 
+    static telegraf = new Telegraf(process.env.TG_BOT_TOKEN || 'null');
+
     static async log(error: any) {
         console.error(error);
+
         let chatId = Number.parseInt(process.env.ADMIN_CHAT_ID ?? '0');
 
-        return tgBot.bot.telegram.sendMessage(chatId, `Error occured: ${error.message}`);
+        return ErrorHandler.telegraf.telegram.sendMessage(chatId, `Error occured: ${error.message}`);
     }
 }
