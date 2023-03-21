9ecd4303482462613e38c0dacc251efa36045b82
Vladimir Prisada <vladimir-prisada@yandex.ru>
2023-03-21 12:38:47 +0100

    Fix tests

--- tests/collector.test.ts
+++ tests/collector.test.ts
@@ -15,9 +15,9 @@ test('Links with filters', () => {
     expect(urls.length).toBe(5);
     expect(urls).toEqual([
             'https://tappancsosotthon.hu/',
-            'https://realestatehungary.hu/szukites/kiado+kisallat-hozhato+uj-epitesu+3-szoba-felett+van-legkondi+butorozott+lakas+budapest+havi-500-ezer-Ft-ig?page=1',
+            'https://ingatlan.com/szukites/kiado+kisallat-hozhato+uj-epitesu+3-szoba-felett+van-legkondi+butorozott+lakas+budapest+havi-500-ezer-Ft-ig?page=1',
             'https://en.alberlet.hu/kiado_alberlet/page:1/haziallat-engedelyezve:igen/ujszeru:igen/szoba:3-x/klima:igen/berendezes:2/ingatlan-tipus:lakas/megye:budapest/berleti-dij:0-500-ezer-ft/limit:100',
-            'https://realestatehungary.hu/szukites/kiado+kisallat-hozhato+van-legkondi+butorozott+haz+budapest+havi-150-250-ezer-Ft?page=1',
+            'https://ingatlan.com/szukites/kiado+kisallat-hozhato+van-legkondi+butorozott+haz+budapest+havi-150-250-ezer-Ft?page=1',
             'https://en.alberlet.hu/kiado_alberlet/page:1/haziallat-engedelyezve:igen/klima:igen/berendezes:2/ingatlan-tipus:haz/megye:budapest/berleti-dij:150-250-ezer-ft/limit:100'
         ]
     );
