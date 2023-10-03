cc5f29a79ecb6e09ca06ab3b882dc45298e778c9
0nua <vladimir.prisada@gmail.com>
2023-10-03 23:02:34 +0200

    Add express types and user agent to the request wrapper

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,13 @@
-const express: any = require('serverless-express/express');
+import {Express} from "express";
+
+interface App extends Express {
+    tgBot: TgBot;
+}
+
+const express = require('serverless-express/express');
 import TgBot from './service/tgBot';
 
-const app = express();
+const app: App = express();
 app.use(express.json());
 
 app.tgBot = new TgBot();
--- src/service/requestWrapper.ts
+++ src/service/requestWrapper.ts
@@ -11,7 +11,20 @@ export default class RequestWrapper {
 
     static config: object = {
         method: 'GET',
-        timeout: 15 * 1000
+        timeout: 15 * 1000,
+        headers: {
+            'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36',
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate",
+            "Connection": "keep-alive",
+            "Upgrade-Insecure-Requests": "1",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "none",
+            "Sec-Fetch-User": "?1",
+            "Cache-Control": "max-age=0"
+        }
     };
 
     static async request(url: string, config: object = {}, repeat: boolean = true): Promise<Response> {
